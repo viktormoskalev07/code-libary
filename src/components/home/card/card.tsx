f9e8dffc815cdf98ed078613238b5c34b5e9b895
Viktor <vmoskalev@smart-ui.pro>
2022-12-05 15:20:20 +0200

    fix card

--- src/components/home/card/card.tsx
+++ src/components/home/card/card.tsx
@@ -11,15 +11,17 @@ interface ICard extends IData{
 export const Card:FC<ICard> = ({title,description,link }) => {
 
 	return <div className={styles.card}>
-		<a target={"_blank"}   rel="noreferrer"  href={link}>
+
      <h2 className={styles.title}>
 		<span  dangerouslySetInnerHTML={{__html:title}}/>
 		 </h2>
 		<p className={styles.desc}> </p>
 		  <span  dangerouslySetInnerHTML={{__html:description}}/>
-			<p >
+		<br/>
+		<a target={"_blank"}   rel="noreferrer"  href={link}>
+
 			link : <span className={styles.link}> {link }  </span>
-		</p>
+
 		</a>
 	</div>
 };
